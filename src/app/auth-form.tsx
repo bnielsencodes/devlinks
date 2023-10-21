23ede762c5a583c96c2895a069d2d4b4ae0dd2e5
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-21 16:49:28 -0700

    Update auth-form.tsx

--- src/app/auth-form.tsx
+++ src/app/auth-form.tsx
@@ -8,29 +8,50 @@ export default function AuthForm() {
   const supabase = createClientComponentClient<Database>();
 
   return (
-    <Auth
-      supabaseClient={supabase}
-      view="magic_link"
-      appearance={{
-        theme: ThemeSupa,
-        style: {
-          divider: {
-            background: "#d9d9d9",
+    <>
+      <Auth
+        supabaseClient={supabase}
+        // show magic link sign-in option
+        view="magic_link"
+        appearance={{
+          theme: ThemeSupa,
+          style: {
+            divider: {
+              background: "#d9d9d9",
+            },
           },
-      }}
-      theme="light"
-      providers={["github", "google"]}
-      redirectTo="http://localhost:3000/auth/callback"
-      showLinks={false}
-      localization={{
-        variables: {
-          magic_link: {
-            email_input_placeholder: "e.g. alex@email.com",
+        }}
+        theme="light"
+        // // no social login options
+        // providers={[]}
+        // social login options
+        providers={["github", "google"]}
+        redirectTo="http://localhost:3000/auth/callback"
+        showLinks={false}
+        // when showLinks, add custom input labels
+        localization={{
+          variables: {
+            magic_link: {
+              email_input_placeholder: "e.g. alex@email.com",
+            },
+            sign_in: {
+              social_provider_text: "Log in with {{provider}}",
+              // email_label: "Email address",
+              // email_input_placeholder: "e.g. alex@email.com",
+              // password_label: "Password",
+              // password_input_placeholder: "Enter your password",
+              // button_label: "Login",
+            },
+            // sign_up: {
+            //   email_label: "",
+            //   email_input_placeholder: "",
+            //   password_label: "Create password",
+            //   password_input_placeholder: "",
+            //   button_label: "Login",
+            // },
           },
-          sign_in: {
-            social_provider_text: "Log in with {{provider}}",
-          },
-      }}
-    />
+        }}
+      />
+    </>
   );
 }
