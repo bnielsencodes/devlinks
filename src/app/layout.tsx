cfb5bf2f77c4120bf75f2cf1f5beee109ec5731a
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-21 16:49:46 -0700

    Update fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,30 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Instrument_Sans } from "next/font/google";
+// import { Instrument_Sans } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Instrument_Sans({ subsets: ["latin"] });
+// const instrumentSans = Instrument_Sans({
+//   subsets: ["latin"],
+//   variable: "--custom-font-instrument-sans",
+// });
+
+const instrumentSans = localFont({
+  src: [
+    {
+      path: "../../public/fonts/static/InstrumentSans-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "../../public/fonts/static/InstrumentSans-SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "../../public/fonts/static/InstrumentSans-Bold.ttf",
+      weight: "700",
+    },
+  ],
+  variable: "--font-instrument-sans",
+});
 
 export const metadata: Metadata = {
   title: "devlinks",
@@ -15,8 +37,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${instrumentSans.variable} font-sans`}>
+      <body
+      // className={`${instrumentSans.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
