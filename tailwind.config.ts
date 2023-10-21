0a34ffafcc39dd5ba1f2c696dccec067529bc845
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-20 22:55:26 -0700

    Add color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        neutral: {
+          100: "#333333",
+          200: "#737373",
+          300: "#D9D9D9",
+          400: "#FAFAFA",
+          500: "#FFFFFF",
+        },
+        primary: {
+          100: "#633CFF",
+          200: "#BEADFF",
+          300: "#EFEBFF",
+        },
+        warning: "#FF3939",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
