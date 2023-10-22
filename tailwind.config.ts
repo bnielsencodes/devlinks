275993a70b877564bce110c292806dc86292f209
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-21 19:56:03 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,36 +7,35 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      neutral: {
+        100: "#333333",
+        200: "#737373",
+        300: "#D9D9D9",
+        400: "#FAFAFA",
+        500: "#FFFFFF",
+      },
+      primary: {
+        100: "#633CFF",
+        200: "#BEADFF",
+        300: "#EFEBFF",
+      },
+      warning: "#FF3939",
+      // placeholderDark: "rgba(255, 255, 255, 0.25)",
+      // placeholderLight: "rgba(0, 1, 18, 0.25)",
+      // palePurpleBtn: "rgba(99, 95, 199, 0.10)",
+    },
+    fontFamily: {
+      sans: ["var(--font-instrument-sans)"],
+    },
     extend: {
       // backgroundImage: {
       //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       //   "gradient-conic":
       //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       // },
-      colors: {
-        neutral: {
-          100: "#333333",
-          200: "#737373",
-          300: "#D9D9D9",
-          400: "#FAFAFA",
-          500: "#FFFFFF",
-        },
-        primary: {
-          100: "#633CFF",
-          200: "#BEADFF",
-          300: "#EFEBFF",
-        },
-        warning: "#FF3939",
-        // placeholderDark: "rgba(255, 255, 255, 0.25)",
-        // placeholderLight: "rgba(0, 1, 18, 0.25)",
-        // palePurpleBtn: "rgba(99, 95, 199, 0.10)",
-      },
-      fontFamily: {
-        sans: ["var(--font-instrument-sans)"],
-      },
     },
   },
-  plugins: [],
 };
 
 export default config;
