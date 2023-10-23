d5cc0f3d5667b4a2d88a9aeed51391a3ac742f88
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2023-10-23 00:21:20 -0700

    Create windowWidth state and side effect

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,4 +1,22 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function Header() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // set windowWidth state on screen size change
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    // add event listener
+    window.addEventListener("resize", handleWindowResize);
+    // remove event listener
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
   return (
     <header className="flex items-center justify-between h-[74px] pr-4 pl-6 mb-4 bg-neutral-500">
     </header>
